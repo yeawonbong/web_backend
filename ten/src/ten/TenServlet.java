9bb6d3e5d0a0d603f38da35b9e4efe12e000b8ea
yeawonbong <ybong.develop@gmail.com>
2022-02-16 09:29:33 +0900

    됐다...dynamic Web app 띄우기 성공

---
+++ ten/src/ten/TenServlet.java
@@ -0,0 +1,21 @@
+package ten;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "TenServlet", value = "/ten")
+public class TenServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        out.print("<h1>1-10까지 출력</h1>");
+        for(int i=1; i <=10; i++) {
+            out.print(i + "<br>");
+        }
+        out.close();
+    }
+}
